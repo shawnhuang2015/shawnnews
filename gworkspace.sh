f4d3ecd755c3e115033cb3cfce5824c6994674f8
Like <like@graphsql.com>
2014-05-22 01:29:03 -0700

    fix workspace script

--- gworkspace.sh
+++ gworkspace.sh
@@ -21,31 +21,35 @@ declare -a BRANCH
 declare -a VERSION
 declare -a SRC_TYPE
 
-build_help=false
+cmd_help=false
 cmd_show_repo=false
 cmd_update_repo=false
+cmd_config_info=false
 CWD=$(pwd)
 #-----------------------------------------------------#
 # check "-d" to see if to build with debug binaries   #
 #-----------------------------------------------------#
 usage() { echo "${bldred}Usage: $0 -h -s -u"
-		  echo "  -h	help and show command list"
-		  echo "  -s	show repository info"
-		  echo "  -u	update or initialize the repositories"
-		  echo "${txtrst}"
+    echo "  -h	help and show command list"
+    echo "  -s	show repository info"
+    echo "  -u	update or initialize the repositories"
+    echo "  -c	config information"
+    echo "${txtrst}"
 }
 
-while getopts ":hus" opt; do
+while getopts ":husc" opt; do
     case $opt in
         h)
-            usage
-            build_help=true
+            cmd_help=true
             ;;
         s)
             cmd_show_repo=true
             ;;
         u)
             cmd_update_repo=true
+            ;;
+        c)
+            cmd_config_info=true
             ;;
         \?)
             echo "Invalid option: -$OPTARG"
@@ -64,8 +68,8 @@ safeRunCommand() {
     typeset ret_code
     now=$(date +"%T")
     if [ $cmd_show_repo == false ]; then
-	    echo "${bldred}[${now}]Running: ${bldblu}${cmnd}${txtrst}"
-	fi
+        echo "${bldred}[${now}]Running: ${bldblu}${cmnd}${txtrst}"
+    fi
     eval $cmnd
     ret_code=$?
     if [ $ret_code != 0 ]; then
@@ -84,122 +88,153 @@ trim() {
     echo -n "$var"
 }
 
+#######################################################
+# Read in repo config file                            #
+#######################################################
 getRepoConfig() {
     i=0
     while read line || [ -n "$line" ] # Read a line
     do
         trimedLine=`trim "$line"`
         if [ $cmd_show_repo == true ]; then
-	        echo "$trimedLine"
-	    fi
+            echo "$trimedLine"
+        fi
         if  ! [[ $trimedLine == \#* ]] && [ -n "$trimedLine" ]; then
             cnf_array[i]="$trimedLine" # Put it into the cnf_array
             i=$(($i + 1))
         fi
     done < $1
-    
-	for str in "${cnf_array[@]}"
-	do
-		IFS=', ' read -a cnfg_details <<< "$str"
-		if [[ "${#cnfg_details[@]}" -lt 6 ]]; then
-			echo "wrong configuration: need at least 6 elements"
-			exit -1
-		fi
-		PUSHABLE=("${PUSHABLE[@]}" "${cnfg_details[0]}")
-		DIRECTORY=("${DIRECTORY[@]}" "${cnfg_details[1]}")
-		REPO=("${REPO[@]}" "${cnfg_details[2]}")
-		BRANCH=("${BRANCH[@]}" "${cnfg_details[3]}")
-		VERSION=("${VERSION[@]}" "${cnfg_details[4]}")
-		SRC_TYPE=("${SRC_TYPE[@]}" "${cnfg_details[5]}")
-	done
+
+    for str in "${cnf_array[@]}"
+    do
+        IFS=', ' read -a cnfg_details <<< "$str"
+        if [[ "${#cnfg_details[@]}" -lt 6 ]]; then
+            echo "wrong configuration: need at least 6 elements"
+            exit -1
+        fi
+        PUSHABLE=("${PUSHABLE[@]}" "${cnfg_details[0]}")
+        DIRECTORY=("${DIRECTORY[@]}" "${cnfg_details[1]}")
+        REPO=("${REPO[@]}" "${cnfg_details[2]}")
+        BRANCH=("${BRANCH[@]}" "${cnfg_details[3]}")
+        VERSION=("${VERSION[@]}" "${cnfg_details[4]}")
+        SRC_TYPE=("${SRC_TYPE[@]}" "${cnfg_details[5]}")
+    done
 }
 
+#######################################################
+# For new project, populate dirs and clone repos      #
+#######################################################
 populate_dir() {
-	for i in "${!DIRECTORY[@]}"; do 
-	    if ! [ -d "${DIRECTORY[$i]}" ]; then
-    	    cmdarr=("${cmdarr[@]}" "mkdir -p ${DIRECTORY[$i]}")
-    	    if ! [ "${PUSHABLE[$i]}" == "x" ]; then
-	    	    cmdarr=("${cmdarr[@]}" "git clone git@github.com:GraphSQL/${REPO[$i]}.git  ${DIRECTORY[$i]}")
-	    	fi
-	    fi
-	done
+    for i in "${!DIRECTORY[@]}"; do 
+        if ! [ -d "${DIRECTORY[$i]}" ]; then
+            cmdarr=("${cmdarr[@]}" "mkdir -p ${DIRECTORY[$i]}")
+            if ! [ "${PUSHABLE[$i]}" == "x" ]; then
+                cmdarr=("${cmdarr[@]}" "git clone git@github.com:GraphSQL/${REPO[$i]}.git  ${DIRECTORY[$i]}")
+            fi
+        fi
+    done
 }
 
+#######################################################
+# Populate or update repos                            #
+#######################################################
 populate_repos() {
-	for i in "${!DIRECTORY[@]}"; do
-		if [ "${VERSION[$i]}" == "latest" ]; then
-            cmdarr=("${cmdarr[@]}" "cd ${DIRECTORY[$i]}; git checkout ${BRANCH[$i]}; git fetch --tags; git pull; cd ${CWD}")
-	    else
-	        if ! [ "${VERSION[$i]}" == "*" ]; then
-		        cmdarr=("${cmdarr[@]}" "cd ${DIRECTORY[$i]}; git checkout ${BRANCH[$i]}; git fetch --tags; git pull; git checkout ${VERSION[$i]} -q; cd ${CWD}")
-		    fi
-	    fi
-	done
+    for i in "${!DIRECTORY[@]}"; do
+        if ! [ "${PUSHABLE[$i]}" == "x" ]; then
+            if [ "${VERSION[$i]}" == "latest" ]; then
+                cmdarr=("${cmdarr[@]}" "cd ${DIRECTORY[$i]}; git checkout ${BRANCH[$i]}; git fetch --tags; git pull; cd ${CWD}")
+            else
+                if ! [ "${VERSION[$i]}" == "*" ]; then
+                    cmdarr=("${cmdarr[@]}" "cd ${DIRECTORY[$i]}; git checkout ${BRANCH[$i]}; git fetch --tags; git pull; git checkout ${VERSION[$i]} -q; cd ${CWD}")
+                fi
+            fi
+        fi
+    done
 }
 
-
+#######################################################
+# Show repos                                          #
+#######################################################
 show_repos() {
-	for i in "${!DIRECTORY[@]}"; do
-		cd "${DIRECTORY[$i]}"
-		cmdarr=("${cmdarr[@]}" "echo; echo ${bldblu}REPO:${bldgre}${REPO[$i]}")
-		cmdarr=("${cmdarr[@]}" "echo ${bldblu}DIR :${bldgre}${DIRECTORY[$i]}")
-    	cmdarr=("${cmdarr[@]}" "echo -ne ${bldblu}TAG :${bldgre}")
-		cmdarr=("${cmdarr[@]}" "cd ${DIRECTORY[$i]}; git name-rev --tags --name-only $(git rev-parse HEAD)")
-	    cmdarr=("${cmdarr[@]}" "echo -ne ${bldblu}SHA :${bldgre}")
-		cmdarr=("${cmdarr[@]}" "git name-rev $(git rev-parse HEAD); cd ${CWD}")
-		cd "${CWD}"
-	done
+    for i in "${!DIRECTORY[@]}"; do
+        cd "${DIRECTORY[$i]}"
+        cmdarr=("${cmdarr[@]}" "echo; echo ${bldblu}REPO:${bldgre}${REPO[$i]}")
+        cmdarr=("${cmdarr[@]}" "echo ${bldblu}DIR :${bldgre}${DIRECTORY[$i]}")
+        cmdarr=("${cmdarr[@]}" "echo -ne ${bldblu}TAG :${bldgre}")
+        cmdarr=("${cmdarr[@]}" "cd ${DIRECTORY[$i]}; git name-rev --tags --name-only $(git rev-parse HEAD)")
+        cmdarr=("${cmdarr[@]}" "echo -ne ${bldblu}SHA :${bldgre}")
+        cmdarr=("${cmdarr[@]}" "git name-rev $(git rev-parse HEAD); cd ${CWD}")
+        cd "${CWD}"
+    done
 }
 
+#######################################################
+# Show current config info. Similar to show_repos but #
+# the output is concise to be included in package     #
+#######################################################
+config_info() {
+    for i in "${!DIRECTORY[@]}"; do
+        cd "${DIRECTORY[$i]}"
+        local timestr=$(git log --pretty=format:%ci -1)
+        local tag=$(git name-rev --tags --name-only $(git rev-parse HEAD))
+        local formatedTag="${tag%^0}"
+        printf '%-20s %-20s %s  ' ${REPO[$i]} $formatedTag $(git rev-parse HEAD)
+        echo "$timestr"
+        cd "${CWD}"
+    done
+}
 
+#######################################################
+# Ignore the other repose as they are read only       #
+#######################################################
 update_gitignore_file()
 {
 
-     # initialize a local var
-     local gitignore_file=".gitignore"
-     local temp_ignore_file=".gitignore_tmp"
+    # initialize a local var
+    local gitignore_file=".gitignore"
+    local temp_ignore_file=".gitignore_tmp"
 
-     if [ ! -f "$gitignore_file" ] ; then
-         # if not create the file
-         touch "$gitignore_file"
-     fi
+    if [ ! -f "$gitignore_file" ] ; then
+        # if not create the file
+        touch "$gitignore_file"
+    fi
 
     local i=0
     while read line || [ -n "$line" ] # Read a line
     do
         trimedLine=`trim "$line"`
-		ignore_array[i]="$trimedLine" # Put ignore into the ignore_array
-		i=$(($i + 1))
+        ignore_array[i]="$trimedLine" # Put ignore into the ignore_array
+        i=$(($i + 1))
     done < $gitignore_file
 
 
-	echo -ne "" > "$temp_ignore_file"
+    echo -ne "" > "$temp_ignore_file"
 
-	# write out existing ignore rules minus other repos
-	for str in "${ignore_array[@]}"
-	do
-	    local found=0
-		for repo_dir in "${DIRECTORY[@]}"
-		do
-			if [ "$str" == "$repo_dir" ]; then
-				found=1
-			fi
-		done
+    # write out existing ignore rules minus other repos
+    for str in "${ignore_array[@]}"
+    do
+        local found=0
+        for repo_dir in "${DIRECTORY[@]}"
+        do
+            if [ "$str" == "$repo_dir" ]; then
+                found=1
+            fi
+        done
 
-		if [ "$found" == 0 ]; then
-			echo "$str" >> "$temp_ignore_file"
-		fi
-	done
+        if [ "$found" == 0 ]; then
+            echo "$str" >> "$temp_ignore_file"
+        fi
+    done
 
-	# write out other repo dirs
-	for i in "${!DIRECTORY[@]}"; do
-		if [ "${PUSHABLE[$i]}" != "+" ] && [ "${PUSHABLE[$i]}" != "x" ]; then
-			echo "${DIRECTORY[$i]}" >> "$temp_ignore_file"
-		fi
-	done
-	
-	#update ignore file
-	mv  $temp_ignore_file $gitignore_file
+    # write out other repo dirs
+    for i in "${!DIRECTORY[@]}"; do
+        if [ "${PUSHABLE[$i]}" != "+" ] && [ "${PUSHABLE[$i]}" != "x" ]; then
+            echo "${DIRECTORY[$i]}" >> "$temp_ignore_file"
+        fi
+    done
+
+    #update ignore file
+    mv  $temp_ignore_file $gitignore_file
 }
 
 
@@ -209,19 +244,28 @@ update_gitignore_file()
 getRepoConfig "proj.config"
 
 if [ $cmd_update_repo == true ]; then
-	update_gitignore_file
-	populate_dir
-	populate_repos
+    update_gitignore_file
+    populate_dir
+    populate_repos
 fi
 
 if [ $cmd_show_repo == true ]; then
-	show_repos
+    show_repos
 fi
 
+if [ $cmd_config_info == true ]; then
+    config_info
+    exit
+fi
+
+if [ ${#cmdarr[@]} == 0 ]; then
+    usage
+    exit
+fi
 
 # run all the commands defines in cmdarr
 for (( i = 0; i < ${#cmdarr[@]} ; i++ )); do
-    if $build_help ; then
+    if $cmd_help ; then
         echo "${bldblu}${cmdarr[$i]}${txtrst}"
     else
         safeRunCommand ${cmdarr[$i]}
