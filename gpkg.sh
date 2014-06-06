7b901887ad38f6fbe8588f0215e394800bd3da9a
Like <like@graphsql.com>
2014-06-05 19:57:24 -0700

    [ER-4]

--- gpkg.sh
+++ gpkg.sh
@@ -2,20 +2,27 @@
 #######################################################
 # build the package                                   #
 #######################################################
+
+# execution function
+safeRunCommand() {
+    typeset cmnd="$*"
+    typeset ret_code
+    now=$(date +"%T")
+    echo "${bldred}[${now}]Running: ${bldblu}${cmnd}${txtrst}"
+    eval $cmnd
+    ret_code=$?
+    if [ $ret_code != 0 ]; then
+        printf "${bldred}Error[%d]: ${bldblu}${cmnd}${txtrst}\n" $ret_code
+        exit $ret_code
+    fi
+}
+
 CWD=$(pwd)
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 cd ${MY_PATH}
-
-${ER_HOME}/tools/saferun.sh "$@" "config/pkg.config"
-
-ret_code=$?
-if [ $ret_code != 0 ]; then
-    printf "Error[%d] when running ${ER_HOME}/tools/saferun.sh config/pkg.config\n" $ret_code
-    exit $ret_code
-else
-    echo "$0 finished successfully!"
-fi
+safeRunCommand "${ER_HOME}/tools/saferun.sh $@ config/pkg.config"
+safeRunCommand "${ER_HOME}/tools/saferun.sh $@ ${ER_HOME}/buildenv/gpkg_other.conf"
 
 cd ${CWD}
 
