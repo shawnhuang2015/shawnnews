308a618260a1333cb6d5c772e0e8380d218c5230
ArneGSQL <arne@graphsql.com>
2014-07-24 14:50:16 -0400

    [NB-3] A tree branch only expands if it has the potential to create a better path.

--- src/customer/core_impl/gpe_impl/bidirect_shortestpath.hpp
+++ src/customer/core_impl/gpe_impl/bidirect_shortestpath.hpp
@@ -21,7 +21,6 @@
 namespace UDIMPL {
   /**************STRUCTS FOR VERTEX AND MESSAGE VALUES************/
   // You can store any fixed-size structure as UDF data. define them here.
-
   /**
    * Defines the type for edge weigts and distances.
    */
@@ -99,7 +98,6 @@ namespace UDIMPL {
       }
   };
 
-
   /**
    * Represents the best known intersection of both search trees. It stores
    * the vertex id and the total distance of the pass throw this vertex.
@@ -146,32 +144,32 @@ namespace UDIMPL {
    * implementation is based on the MinVariable class (src/core/gpe/gpelib4/enginebase/variable.hpp).
    * The purpuse is to provide a minimum-aggregator for a global IntersectionInfo variable.
    */
-  class IntersectionInfoVariable : public BasicTypeVariableObject<IntersectionInfo> {
-   public:
+  class IntersectionInfoVariable: public BasicTypeVariableObject<IntersectionInfo> {
+    public:
       IntersectionInfoVariable() {
-      this->value_ = IntersectionInfo();
-    }
+        this->value_ = IntersectionInfo();
+      }
 
-    explicit IntersectionInfoVariable(IntersectionInfo value) {
-      this->value_ = value;
-    }
+      explicit IntersectionInfoVariable(IntersectionInfo value) {
+        this->value_ = value;
+      }
 
-   protected:
-    BaseVariableObject* MakeLocalCopy() const {
-      return new IntersectionInfoVariable();
-    }
+    protected:
+      BaseVariableObject* MakeLocalCopy() const {
+        return new IntersectionInfoVariable();
+      }
 
-    void Reduce(const void* newvalue) {
-      const IntersectionInfo* value = reinterpret_cast<const IntersectionInfo*>(newvalue);
-      if ((*value) < this->value_)
-        this->value_ = (*value);
-    }
+      void Reduce(const void* newvalue) {
+        const IntersectionInfo* value = reinterpret_cast<const IntersectionInfo*>(newvalue);
+        if ((*value) < this->value_)
+          this->value_ = (*value);
+      }
 
-    void Combine(BaseVariableObject* localcopy) {
-      IntersectionInfoVariable* obj = (IntersectionInfoVariable*) localcopy;
-      if (obj->value_ < this->value_)
-        this->value_ = obj->value_;
-    }
+      void Combine(BaseVariableObject* localcopy) {
+        IntersectionInfoVariable* obj = (IntersectionInfoVariable*) localcopy;
+        if (obj->value_ < this->value_)
+          this->value_ = obj->value_;
+      }
   };
 
   /**************Enumerations and Constants************/
@@ -242,7 +240,6 @@ namespace UDIMPL {
        */
       unsigned long int weightIndex_;
 
-
     public:
       // UDF Settings: Computation Modes
       static const gpelib4::EngineProcessingMode EngineMode = gpelib4::EngineProcessMode_ActiveVertices;
@@ -258,7 +255,6 @@ namespace UDIMPL {
       typedef topology4::EdgeAttribute_Writable E_ATTR;
       typedef SearchTreeInfo V_VALUE;
       typedef SearchTreeInfo MESSAGE;
-
 
       /**************Class Constructor************/
       BidirectionalShortestPath(VertexLocalId_t sourceId, VertexLocalId_t targetId, unsigned long int weightIndex) :
@@ -328,26 +324,40 @@ namespace UDIMPL {
        *
        ************/
       ALWAYS_INLINE void EdgeMap(const VertexLocalId_t& srcId, V_ATTR* srcAttr, const V_VALUE& srcValue,
-          const VertexLocalId_t& tarId, V_ATTR* tarAttr, const V_VALUE& tarValue,
-          E_ATTR* edgeAttr, gpelib4::SingleValueMapContext<MESSAGE>* context) {
+          const VertexLocalId_t& tarId, V_ATTR* tarAttr, const V_VALUE& tarValue, E_ATTR* edgeAttr,
+          gpelib4::SingleValueMapContext<MESSAGE>* context) {
 
         unsigned long int weightIndex = context->GlobalVariable_GetValue<unsigned long int>(GV_WEIGHT_INDEX);
         WEIGHT edgeWeight = edgeAttr->GetUInt32(weightIndex, 0);
 
         WEIGHT intDist = context->GlobalVariable_GetValue<IntersectionInfo>(GV_INTERSECTION_VERTEX).distance;
+        WEIGHT currMinSDist = context->GlobalVariable_GetValue<WEIGHT>(GV_CURRENT_MIN_ACTIVE_WEIGHT_S);
+        WEIGHT currMinTDist = context->GlobalVariable_GetValue<WEIGHT>(GV_CURRENT_MIN_ACTIVE_WEIGHT_T);
 
         // s-Tree
-        WEIGHT newSDist = srcValue.sDist + edgeWeight;
-        if (srcValue.sPrev != tarId && tarValue.sDist > newSDist && newSDist <= intDist) {
-          context->Write(tarId, MESSAGE(srcId, newSDist, -1, MAX_WEIGHT));
-          context->GlobalVariable_Reduce(GV_NEXT_MIN_ACTIVE_WEIGHT_S, newSDist);
+        if (srcValue.sDist < MAX_WEIGHT) {
+          //Vertex has been dicovered by s-tree
+          WEIGHT newSDist = srcValue.sDist + edgeWeight;
+
+          bool expandSTree = ShouldVertexEpandTree(srcValue, currMinTDist, intDist, true);
+
+          if (srcValue.sPrev != tarId && expandSTree && tarValue.sDist > newSDist && newSDist <= intDist) {
+            context->Write(tarId, MESSAGE(srcId, newSDist, -1, MAX_WEIGHT));
+            context->GlobalVariable_Reduce(GV_NEXT_MIN_ACTIVE_WEIGHT_S, newSDist);
+          }
         }
 
         // t-Tree
-        WEIGHT newTDist = srcValue.tDist + edgeWeight;
-        if (srcValue.tPrev != tarId && tarValue.tDist > newTDist && newTDist <= intDist) {
-          context->Write(tarId, MESSAGE(-1, MAX_WEIGHT, srcId, newTDist));
-          context->GlobalVariable_Reduce(GV_NEXT_MIN_ACTIVE_WEIGHT_T, newTDist);
+        if (srcValue.tDist < MAX_WEIGHT) {
+          //Vertex has been dicovered by t-tree
+          WEIGHT newTDist = srcValue.tDist + edgeWeight;
+
+          bool expandTTree = ShouldVertexEpandTree(srcValue, currMinSDist, intDist, false);
+
+          if (srcValue.tPrev != tarId && expandTTree && tarValue.tDist > newTDist && newTDist <= intDist) {
+            context->Write(tarId, MESSAGE(-1, MAX_WEIGHT, srcId, newTDist));
+            context->GlobalVariable_Reduce(GV_NEXT_MIN_ACTIVE_WEIGHT_T, newTDist);
+          }
         }
 
       }
@@ -377,8 +387,10 @@ namespace UDIMPL {
         context->GlobalVariable_Reduce(GV_CURRENT_MIN_ACTIVE_WEIGHT_S, nextMinSDist);
         context->GlobalVariable_Reduce(GV_CURRENT_MIN_ACTIVE_WEIGHT_T, nextMinTDist);
 
-        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_S))->Set(MAX_WEIGHT);
-        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_T))->Set(MAX_WEIGHT);
+        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_S))->Set(
+            MAX_WEIGHT);
+        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_T))->Set(
+            MAX_WEIGHT);
 
       }
 
@@ -430,13 +442,17 @@ namespace UDIMPL {
 
         if (localIntDist < globalIntDist) {
           globalIntDist = localIntDist;
-          context->GlobalVariable_Reduce<IntersectionInfo>(GV_INTERSECTION_VERTEX, IntersectionInfo(verId, localIntDist));
+          context->GlobalVariable_Reduce<IntersectionInfo>(GV_INTERSECTION_VERTEX,
+              IntersectionInfo(verId, localIntDist));
         }
 
-        // Since EdgeMap only send a message to a vertex if its distance decreases, Reduce is also
-        // only called if the distance decreases. Therefore, we can activate the vertex for the
-        // next iteration without additional checks.
-        context->SetActiveFlag(verId);
+        WEIGHT currMinSDist = context->GlobalVariable_GetValue<WEIGHT>(GV_CURRENT_MIN_ACTIVE_WEIGHT_S);
+        WEIGHT currMinTDist = context->GlobalVariable_GetValue<WEIGHT>(GV_CURRENT_MIN_ACTIVE_WEIGHT_T);
+
+        bool expandSTree = ShouldVertexEpandTree(vVal, currMinTDist, globalIntDist, true);
+        bool expandTTree = ShouldVertexEpandTree(vVal, currMinSDist, globalIntDist, false);
+
+        context->SetActiveFlag(expandSTree || expandTTree);
 
       }
 
@@ -456,8 +472,10 @@ namespace UDIMPL {
         context->GlobalVariable_Reduce(GV_CURRENT_MIN_ACTIVE_WEIGHT_S, nextMinSDist);
         context->GlobalVariable_Reduce(GV_CURRENT_MIN_ACTIVE_WEIGHT_T, nextMinTDist);
 
-        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_S))->Set(MAX_WEIGHT);
-        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_T))->Set(MAX_WEIGHT);
+        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_S))->Set(
+            MAX_WEIGHT);
+        reinterpret_cast<gpelib4::MinVariable<WEIGHT>*>(context->GetGlobalVariable(GV_NEXT_MIN_ACTIVE_WEIGHT_T))->Set(
+            MAX_WEIGHT);
 
       }
 
@@ -471,10 +489,9 @@ namespace UDIMPL {
       void EndRun(gpelib4::BasicContext* context) {
         globalIntDist_ = context->GlobalVariable_GetValue<IntersectionInfo>(GV_INTERSECTION_VERTEX).distance;
 
-
       }
 
-      WEIGHT getDistance(){
+      WEIGHT getDistance() {
         return globalIntDist_;
       }
 
@@ -489,7 +506,6 @@ namespace UDIMPL {
       ALWAYS_INLINE void Write(gutil::GOutputStream& ostream, const VertexLocalId_t& vid, V_ATTR* vertexattr,
           const V_VALUE& singlevalue, gpelib4::BasicContext* context) {
 
-
       }
 
       /**
@@ -500,43 +516,55 @@ namespace UDIMPL {
         return "";
       }
 
-      private:
+    private:
       WEIGHT globalIntDist_;
+
+      /**
+       * Determins if exploring the tree from the given vertex can lead to a shorter path than the
+       * best currently known.
+       */
+      bool ShouldVertexEpandTree(const V_VALUE& verValue, WEIGHT currMinActiveBackDist, WEIGHT bestKnownInterDist,
+          bool isSTree) {
+
+        WEIGHT forwardDist = isSTree ? verValue.sDist : verValue.tDist;
+        WEIGHT backDist = isSTree ? verValue.tDist : verValue.sDist;
+
+        // Lower bound for the distance in the back-tree.
+        // ToDo: Implement the case that v is in (ii) of back-tree,
+        //       i.e. backDist > currMinActiveBackDist but we know that it is optimal.
+        WEIGHT bestBackDist = std::min(currMinActiveBackDist, backDist);
+
+        return forwardDist + bestBackDist < bestKnownInterDist;
+
+      }
 
   };
 
-  /********************UDF Runner Registry Object *************************/
+/********************UDF Runner Registry Object *************************/
 ////template <typename VID_t, typename V_ATTR_t, typename E_ATTR_t>
 //  class BidirectionalShortestPathRunner: public UDFRunner {
 //    public:
 //      typedef BidirectionalShortestPath UDF_t;
-
 //      BidirectionalShortestPathRunner() {
 //        requires<VertexLocalId_t>("sourceId", "s", "The vertex id of the source vertex.", &sourceId, 0);
 //        requires<VertexLocalId_t>("targetId", "t", "The vertex id of the target vertex.", &targetId, 0);
 //        requires<VertexLocalId_t>("atrrIndex", "i", "The index of the edge attribute containing the edge weight.", &atrrIndex, 0);
 //      }
-
 //      ~BidirectionalShortestPathRunner() {
 //      }
-
 //      void createUDF() {
 //        udf = new UDF_t(sourceId, targetId, atrrIndex);
 //      }
-
 //      void runUDF(std::string engineConfigFile, std::string topologyConfigFile, std::string outputFile) {
 //        gpelib4::EngineDriver driver(engineConfigFile, topologyConfigFile);
 //        driver.RunSingleVersion<UDF_t>((UDF_t*) udf, outputFile);
 //      }
-
 //      static void registerUDF(UDFRegistry& registry) {
 //        registry.add("BidirectionalShortestPath", new BidirectionalShortestPathRunner());
 //      }
-
 //    private:
 //      VertexLocalId_t sourceId;
 //      VertexLocalId_t targetId;
 //      unsigned int atrrIndex;
-
 //  };
-} //namespace gperun
+}//namespace gperun
