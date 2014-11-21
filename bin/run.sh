cf47199ad0860736f63236a054dd310730551852
Mingxi Wu <mingxi.wu@graphsql.com>
2014-11-21 19:42:05 +0000

    [GS-1] add working dir creation shell script

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+WORKDIR=./work
+if [ ! -d "$WORKDIR" ]; then
+  mkdir  "$WORKDIR"
+fi
+OUTPUT=./output
+if [ ! -d "$OUTPUT" ]; then
+  mkdir  "$OUTPUT"
+fi
+
 JAR=gsql.jar
 
 java -jar "$JAR" config 
