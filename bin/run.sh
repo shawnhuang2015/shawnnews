2a82e50d6e1a667b0794fbf54182896be4683ed2
Mingxi Wu <mingxi.wu@graphsql.com>
2014-11-22 09:01:35 +0000

    [GS-1] export LD_LIBRARY_PATH during startup

--- bin/run.sh
+++ bin/run.sh
@@ -4,6 +4,9 @@ if [ ! -d "$WORKDIR" ]; then
   mkdir  "$WORKDIR"
 fi
 
+export LD_LIBRARY_PATH='/vagrant/repo2/product/bin/'
+echo "LD_LIBRARY_PATH="$LD_LIBRARY_PATH
+
 JAR=gsql.jar
 
 java -jar "$JAR" config 
