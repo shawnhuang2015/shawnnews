9a220b6bdd5f81a97e8c4267b3947d69bb3894d6
Mingxi Wu <mingxi.wu@graphsql.com>
2015-03-04 05:49:42 +0000

    [GS-1] add example of tuple token function

--- src/customer/core_impl/gsql_impl/TokenBank/TokenBank1.cpp
+++ src/customer/core_impl/gsql_impl/TokenBank/TokenBank1.cpp
@@ -42,6 +42,24 @@
  *       @param: iTokenLen: the input token length 
  *       @return: converted value
  *
+ *
+ *   3. string -> string 
+ *
+ *      The UDF token conversion functions will take an input char array and do 
+ *      a customized conversion. Then, put the converted char array to output 
+ *      char buffer. This converted char array is a concatenation of multiple
+ *      tuples.
+ *
+ *      extern "C" void funcName (const char* const iToken, uint32_t iTokenLen, 
+ *                           char *const oToken, uint32_t& oTokenLen);
+ *
+ *       @param: iToken: 1 input token pointed by one char pointer.
+ *       @param: iTokenLen: the input token length 
+ *       @param: oToken: the output token buffer; caller will prepare this buffer
+ *       @param: oTokenLen: the output token length 
+ *
+ *     Note: each tuple must be end with (char)30. For field separator within tuple,
+ *     we use (char)31. See below example ratingMovie()
  * 
  *  Think token function as a UDF designed to transform a specific column before we load 
  *  it in graph store.
@@ -71,7 +89,10 @@
 #include <stdint.h>
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <string>
 
+using namespace std;
 /**
  * This function convert iToken char array of size iTokenLen, reverse order 
  * and put it in oToken.
@@ -81,7 +102,7 @@ extern "C"  void Reverse (const char* const iToken, uint32_t iTokenLen,
     char *const oToken, uint32_t& oTokenLen) {
 
   uint32_t j = 0;
-   for (uint32_t i = iTokenLen; i>0; i--){
+  for (uint32_t i = iTokenLen; i>0; i--){
     oToken[j++] = iToken[i-1];     
   }
   oTokenLen = j;
@@ -93,6 +114,81 @@ extern "C"  void Reverse (const char* const iToken, uint32_t iTokenLen,
  */
 extern "C"  uint64_t Zero(const char* const iToken, uint32_t iTokenLen) {
   return 0;
+}
+
+
+/**
+ * Suppose I have a schema, and the third column is 
+ * rating:movieId1;movieId2..|rating:movieId3,movieId4..
+ *
+ * id,name, p_rate_movie
+ * 1,Charlie Sheen,3:1;4|9:2
+ *
+ * The below function will take token from the p_rate_movie column.
+ * And produce 
+ * 3 (char)31 1 (char)30 3 (char)31 4 (char)30 9 (char)31 2 (char)30
+ *
+ * where 31 and 30 are ascii invisible chars. 31 is field separator, 
+ * 30 is record separator.
+ */
+extern "C" void ratingMovie(const char* const iToken, uint32_t iTokenLen, 
+    char *const oToken, uint32_t& oTokenLen) {
+
+  int i = 0; //start point
+
+  int j = 0; //current cursor
+
+  oTokenLen = 0;
+
+  while (j<iTokenLen) {
+    //find one field
+    while (j < iTokenLen ) {
+
+      if (iToken[j] != '|'){
+        j ++;
+      } else {
+        break; 
+      }
+    }
+
+    //start is i, end is j, find rating
+    int r;
+    for (int m = i; m < j; m++) {
+      if (iToken[m] == ':') {
+        r = m;
+        break;
+      }
+    }
+
+    //from r+1 to j, construct tuples
+    bool newTuple = true;
+    for (int m = r+1; m < j; m++) {
+
+      //create rating field
+      if(newTuple){
+        newTuple = false ;
+        //get first field
+        for (int k = i; k<r; k++){
+          oToken[oTokenLen ++] = iToken[k];
+        }
+        //append field separator
+        oToken[oTokenLen++] = (char)31;
+      } 
+
+      if (iToken[m] != ';') {
+        oToken[oTokenLen ++] = iToken[m];
+      } else {
+        oToken[oTokenLen ++] = (char)30;
+        newTuple = true;
+      }
+    }
+
+    oToken[oTokenLen ++] = (char)30;
+
+    i = j+1;
+    j++;
+  }
+
 }
 
 
@@ -121,6 +217,24 @@ int main(){
     std::cout<<b[i]<<",";
   }
   std::cout<<len<<std::endl;
+
+  char c[14] = {'4',':','1',',','2','|','5',':','3','|','1','0',':','4'};
+
+  uint32_t alen = 0; 
+  ABC(c,14, b, alen);
+
+  cout<<"alen= "<<alen<<endl;
+
+  for (int i = 0; i < alen; i++) {
+     if (b[i] == (char) 31) {
+         cout <<","; 
+     } else if (b[i] == (char) 30){
+         cout <<"|"; 
+     } else {
+       cout <<b[i];
+     }
+  }
+  cout <<endl;
 
 }
 
