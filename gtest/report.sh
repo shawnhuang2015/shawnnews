2d990cfef0a71646ca9ecdefa00607638e71b5a6
Mingxi Wu <mingxi.wu@graphsql.com>
2015-05-06 02:15:15 -0400

    add report.sh

--- gtest/report.sh
+++ gtest/report.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 for file in ./diff/*/regress*/*.diff; do
+  echo "=========================================="
+  echo cat diffs: $file
+  echo "=========================================="
+  cat $file
   file2=${file/diff/output}
-  echo cat ${file2/diff/log}
+  echo "=========================================="
+  echo cat logs:  ${file2/diff/log}
+  echo "=========================================="
   cat ${file2/diff/log}
 done
