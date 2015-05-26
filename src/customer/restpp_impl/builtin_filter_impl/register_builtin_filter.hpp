54b67860f4ed45124c21bba62110379ca3c38f01
Jianlong@Vagrant <jianlong.zhong@graphsql.com>
2015-05-26 17:49:51 +0000

    [RESTPP-BASE] add instructions on wrirting builtin filters

--- src/customer/restpp_impl/builtin_filter_impl/register_builtin_filter.hpp
+++ src/customer/restpp_impl/builtin_filter_impl/register_builtin_filter.hpp
@@ -1,29 +1,30 @@
-#ifndef REGISTER_BUILTIN_FILTER_HPP
-#define REGISTER_BUILTIN_FILTER_HPP
-
-
 /******************************************************************************
  * Copyright (c) 2015, GraphSQL Inc.
  * All rights reserved.
- * Project: GHTTP
+ * Project: RESTPP
  * register_builtin_filter.hpp:
  *
  *  Created on: Apr 24, 2015
  *      Author: jzhong
+ *  See https://graphsql.atlassian.net/wiki/pages/viewpage.action?pageId=65831094
+ *  (Section 3.5) for details about using builtin filters.
  ******************************************************************************/
+#ifndef REGISTER_BUILTIN_FILTER_HPP
+#define REGISTER_BUILTIN_FILTER_HPP
+
 #include <sysinit.hpp>
 #include <filtermanager.hpp>
 #include <parse.hpp>
 
 /*----------------------------------------------------------------------------------------
-Instruction:  All filter code are included in this file and compiled into one object file.
-              Include your .cpp here.
+Ste 1:  All filter code are included in this file and compiled into one object file.
+        Include your .cpp here.
 -----------------------------------------------------------------------------------------*/
 
 
 void RegisterBuiltinFilters(){
   /*--------------------------------------------------------------------------------------
-  Instruction: register the function pointer of your filter with the filter name.
+  Step 2: register the function pointer of your filter with the filter name.
   Register your filter here by calling:
   RESTPP_COMPONENTS::filtermanager->RegisterBuiltinFilter("FilterName", (UpdateRequestFilter)&FilterName);
   ---------------------------------------------------------------------------------------*/
