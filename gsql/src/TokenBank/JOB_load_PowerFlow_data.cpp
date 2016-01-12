068e8783aa7cdd855144c7df7861616dd6172dc6
shawn.huang <shawn.huang@graphsql.com>
2016-01-12 12:48:39 -0800

    Config the Query language evironment.

---
+++ gsql/src/TokenBank/JOB_load_PowerFlow_data.cpp
@@ -0,0 +1,8 @@
+#include "ConditionLibM.hpp"
+#include <cmath>
+extern "C" bool load_PowerFlow_data_loadVertexQb_1452621612289_0 (const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenNum) {
+	std::string variable0(iToken[0], iTokenLen[0]);
+	return  (variable0) !=  ("NaN");
+}
+
+
