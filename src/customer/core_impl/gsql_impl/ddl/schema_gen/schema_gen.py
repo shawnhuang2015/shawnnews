c8d93480b203ad77cef4000268b42d2c3aee0bb0
alan.lee <alan.lee@graphsql.com>
2016-01-25 15:13:35 +0800

    CIP-11, create_graph.

--- src/customer/core_impl/gsql_impl/ddl/schema_gen/schema_gen.py
+++ src/customer/core_impl/gsql_impl/ddl/schema_gen/schema_gen.py
@@ -5,6 +5,7 @@ def create_vertex(l):
   v_template = 'create vertex {vtype} (primary_id {vtype}_id string not null, {attrs}) with stats="outdegree_by_edgetype"'
   attr_template = '{name} {dtype}'
 
+  vtypes = []
   vs = []
   for i in l:
     attrs = []
@@ -15,12 +16,44 @@ def create_vertex(l):
       attrs.append(a)
     attrs = ', '.join(attrs)
     vs.append(v_template.format(vtype=i['type'], attrs=attrs))
-  return vs
+    vtypes.append(i['type'])
+  return vs, vtypes
+
+def create_edge(l):
+  e_template = 'create {directed} edge {etype} (from {source}, to {target}, {attrs})'
+  attr_template = '{name} {dtype}'
+
+  etypes = []
+  es = []
+  for i in l:
+    attrs = []
+    for _ in i['attr']:
+      a = attr_template.format(name=_['name'], dtype=_['dtype'])
+      if 'default' in _:
+        a += ' default {}'.format(_['default'])
+      attrs.append(a)
+    attrs = ', '.join(attrs)
+    es.append(e_template.format(
+      directed='directed' if i['directed'] else 'undirected', 
+      etype=i['type'], 
+      attrs=attrs,
+      source=i['source'],
+      target=i['target']))
+    etypes.append(i['type'])
+  return es, etypes
+
+def create_graph(schema_json):
+  with open('ui_schema.json', 'r') as fp:
+    f = json.load(fp)
+    vs, vtypes = create_vertex(f['vertex'])
+    es, etypes = create_edge(f['edge'])
+    graph = ['drop all']
+    graph.extend(vs)
+    graph.extend(es)
+    graph.append('create graph cip ({})'.format(','.join(vtypes + etypes)))
+    graph.append('export schema cip')
+  return graph
 
 
 if __name__ == '__main__':
-  with open('ui_schema.json', 'r') as fp:
-    f = json.load(fp)
-    vs = create_vertex(f['vertex'])
-    print '\n'.join(vs)
-
+  print '\n'.join(create_graph('ui_schema.json'))
