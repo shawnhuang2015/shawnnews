840567ac9371965e8cb3520780bbf54d23937589
alan.lee <alan.lee@graphsql.com>
2016-01-30 12:29:22 +0800

    CIP-11, piece togother.

---
+++ src/customer/core_impl/gsql_impl/ddl/schema_gen/run.sh
@@ -0,0 +1,27 @@
+
+# generate json schema
+python ui_schema_fake.py | python -m json.tool > ui_schema.json
+
+# json schema -> gsql schema
+python schema_gen.py ui_schema.json > schema.gsql
+
+# schema -> post api
+python post_api_gen.py schema.gsql > post_api.gsql
+
+# piece together
+cp schema.gsql post_api.gsql /tmp/
+
+cat > /tmp/online_ddl.gsql <<EOF
+@/tmp/schema.gsql
+
+EOF
+
+cat post_api.gsql >> /tmp/online_ddl.gsql
+
+cat >> /tmp/online_ddl.gsql <<EOF
+
+clear graph store -HARD
+init graph store for graph cip
+
+EOF
+
