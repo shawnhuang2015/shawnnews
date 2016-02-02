b78c24bbdab1e43af49f14c514ed3da0dfec8ff3
feng.chen <feng.chen@graphsql.com>
2016-02-02 11:53:05 +0800

    REM-6 add mem cache

---
+++ cip/ruleEngD/init/Const.py
@@ -0,0 +1,10 @@
+POSTFIX_META = "_meta"
+POSTFIX_PKG = "_pkg"
+POSTFIX_TOPO = "_topo"
+SUITE_ROOT = "/home/feng.chen/gitrepo/product/cip/ruleEngD/rulesuites"
+SUITE_NAME_DEF="rulesuite"
+
+REST_OK=200
+REST_BAD_REQ=400
+REST_INTERNAL_ERROR=500
+
