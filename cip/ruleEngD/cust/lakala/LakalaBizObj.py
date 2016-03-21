c914f4b706fd5d6ddedc62455ce9f53bb7905e92
feng.chen <feng.chen@graphsql.com>
2016-03-22 06:32:37 +0800

    fix lakala gpath

--- cip/ruleEngD/cust/lakala/LakalaBizObj.py
+++ cip/ruleEngD/cust/lakala/LakalaBizObj.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("../")
+sys.path.append("../..")
 from config.RuleEngConfig import ruleEngConfigure
 ########  common biz objects #########################
 from rulebase.GPathBizObj import GPathBizObj
@@ -10,13 +11,28 @@ class LakalaBizObjManager(object):
         self.config = GPathConfig(ruleEngConfigure["gstore"]["host"], ruleEngConfigure["gstore"]["port"])
 
     def getMerchantTrans(self, merc_id, tran_status, ts_start, ts_end):
-        gpath = "Merchant[\"%s\"]-Merchant_Txn-Txn{Txn.trans_status=\"%s\"  and Txn.trans_time > %s and  Txn.trans_end < %s}{COUNT(Txn._external_id)}" %s (merc_id, tran_status, ts_start, ts_end)
+        gpath = "Merchant[\"%s\"]-Merchant_Txn->Txn{trans_status == \"%s\"  and trans_time > %s and trans_time < %s }=>.group{1 as all}[COUNT(Txn._internal_id) as cnt]" % (merc_id, tran_status, ts_start, ts_end)
         gpathObj = GPathBizObj(self.config, gpath)
-        return gpathObj.getList()[0]
+        count = 0
+        if gpathObj.getList() != []:
+            count = gpathObj.getList()[0][1]
+        return count
 
     def getMercCardTranAmount(self, card_no, ts_start, ts_end):
-        gpath = "MercCard[\"%s\"]-CardApp_MercCard-CardApp-Merchant_CardApp-Merchant-Merchant_Txn-Txn{Txn.trans_time > %s and Txn.trans_end < %s}{SUM(Txn.trans_amt)}" %s (merc_id, tran_status, ts_start, ts_end)
+        gpath = "MercCard[\"%s\"]-CardApp_MercCard->CardApp-Merchant_CardApp->Merchant-Merchant_Txn->Txn{Txn.trans_time > %s and Txn.trans_end < %s}=>.group{1 as all}[SUM(Txn.trans_amt)]" %s (card_no, ts_start, ts_end)
         gpathObj = GPathBizObj(self.config, gpath)
-        return gpathObj.getList()[0]
+        amount = 0
+        if gpathObj.getList() != []:
+            amount = gpathObj.getList()[0][1]
+        return amount
 
 lakalaBizObjManager = LakalaBizObjManager()
+
+if __name__ == "__main__":
+    config = GPathConfig(ruleEngConfigure["gstore"]["host"], ruleEngConfigure["gstore"]["port"])
+    gpath = "geoHash[\"uxvpgrypyxyzbpuxuzbrbxgzzzzzzzzz\"]-userActivationEvent_geoHash->userActivationEvent{ts == 0 and gs_touch_time == 0}=>.group{1 as all}[COUNT(userActivationEvent.ts) as cnt]"
+    gpathObj = GPathBizObj(config, gpath)
+    count = 0
+    if gpathObj.getList() != []:
+        count = gpathObj.getList()[0][1]
+    print count
