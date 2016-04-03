51a549ec9338d88f78a5b4fac1b95081fec3d39f
shawn.huang <shawn.huang@graphsql.com>
2016-04-03 15:34:56 -0400

    [CIP-56] Create the porject.

--- cip/cip_frontend/client/app/app.js
+++ cip/cip_frontend/client/app/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('cipApp', [
-  'cipApp.auth',
-  'cipApp.admin',
-  'cipApp.constants',
+  // 'cipApp.auth',
+  // 'cipApp.admin',
+  // 'cipApp.constants',
   'ngCookies',
   'ngResource',
   'ngSanitize',
@@ -11,9 +11,42 @@ angular.module('cipApp', [
   'ui.bootstrap',
   'validation.match'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  });
+angular
+.module('cipApp')
+.config(config)
+.run(function($rootScope, $state) {
+    $rootScope.$state = $state;
+});
+
+function config($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise("/index/main");
+
+  $stateProvider
+  .state('index', {
+      abstract: true,
+      url: "/index",
+      templateUrl: "template/views/common/content.html",
+      controller: 'mainCtrl',
+      controllerAs: 'main'
+  })
+  .state('index.main', {
+      url: "/main",
+      templateUrl: "template/views/main.html",
+      data: { pageTitle: 'Example view index main' }
+  })
+  .state('index.create_single_crowd', {
+      url: "/create_single_crowd",
+      templateUrl: "template/views/main.html",
+      data: { pageTitle: 'create_single_crowd' }
+  })
+  .state('index.minor', {
+      url: "/minor",
+      templateUrl: "template/views/minor.html",
+      data: {},
+      controller: 'minorCtrl',
+      controllerAs : 'minor'
+  })
+
+  $locationProvider.html5Mode(true);
+}
