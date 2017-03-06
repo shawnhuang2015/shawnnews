f2092c2564c9374feaeb76a909eda41b3028bcee
shawn.huang <shawn.huang@graphsql.com>
2017-03-06 14:20:10 -0800

    test the class overrider.

--- apps/src/app/foot/foot.component.ts
+++ apps/src/app/foot/foot.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-foot',
   templateUrl: './foot.component.html',
-  styleUrls: ['./foot.component.scss']
+  styleUrls: ['./foot.component.scss'],
+  host: {
+    'class': 'testhost'
+  }
 })
 
 export class FootComponent implements OnInit {
-  constructor() {
+  @Input() value2: string;
 
+  constructor() {
+    // this.value2 = 'default';
   }
 
   ngOnInit() {
