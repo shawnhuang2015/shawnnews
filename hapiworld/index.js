9ed83b42d27c51ca696fc6bb8bdfc0862f59f62b
shawn.huang <shawn.huang@graphsql.com>
2017-05-21 17:45:28 -0700

    Add hapi hello world

---
+++ hapiworld/index.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var hapi = require("hapi");
+var server = new hapi.Server();
+server.connection({ port: 3000 });
+server.route({
+    method: "GET",
+    path: "/",
+    handler: function (request, reply) {
+        reply("Hello World !!");
+    }
+});
+server.start(function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("server running at 3000");
+});
